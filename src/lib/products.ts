70536eee1b5952e17ab0c49664f055d1816a6789
Krish <krishnabaviskar04@gmail.com>
2025-08-24 22:50:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -320,7 +320,7 @@ const products: Product[] = [
     description: "Experience feather-light runs with the AeroGlide Running Shoes. Designed for both speed and comfort, they feature a responsive foam sole and a breathable mesh upper to keep your feet cool and supported over any distance.",
     price: 3499.00,
     rating: 4.7,
-    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/f/k/v/8-nitro-802-8-action-light-grey-navy-original-imahfphzbjcbjckc.jpeg?q=70",
+    image: "https://rukminim2.flixcart.com/image/612/612/l4ssfww0/shoe/o/b/6/13-arm122-47-ferar-curo-black-original-imagfm9yfby8ygau.jpeg?q=70",
     category: "Fashion",
     specifications: {
       "Type": "Running Shoes",
