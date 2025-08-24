d535a4d3e2b191e8b5571f82063effd93b4c54e0
Krish <krishnabaviskar04@gmail.com>
2025-08-24 20:39:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 export function Banner() {
-    const bannerImage = "https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=1600&auto=format&fit=crop";
+    const bannerImage = "https://rukminim2.flixcart.com/fk-p-flap/1620/270/image/e5117445d2fc66ca.jpeg?q=80";
     const fallbackBanner = "https://placehold.co/1600x270.png";
 
     return (
