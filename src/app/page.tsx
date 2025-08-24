0fae04fd01d5af3a60836a44ebc1524e943d344f
Krish <krishnabaviskar04@gmail.com>
2025-08-24 21:12:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import { SecondaryBanner } from "@/components/secondary-banner";
 export default function Home() {
   const products: Product[] = getProducts();
   const topPicks = [...products].sort((a, b) => b.rating - a.rating);
+  const electronics = products.filter(p => p.category === "Electronics" || p.category === "Mobiles" || p.category === "Gaming");
+  const home = products.filter(p => p.category === "Home & Furniture" || p.category === "Home & Garden" || p.category === "Appliances");
+  const fashion = products.filter(p => p.category === "Fashion");
   
   return (
     <div className="space-y-4">
@@ -17,7 +20,10 @@ export default function Home() {
       </div>
       <div className="container mx-auto px-4 space-y-8">
         <ProductList products={products} title="Deals of the Day"/>
+        <ProductList products={electronics} title="Best of Electronics" />
         <SecondaryBanner />
+        <ProductList products={home} title="Top Picks for Your Home" />
+        <ProductList products={fashion} title="Top Fashion Picks" />
         <ProductList products={topPicks} title="Top Picks" />
         <ProductList products={[...products].reverse()} title="Recommended for You" />
       </div>
