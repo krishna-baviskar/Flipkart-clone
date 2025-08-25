0bde590c23a405e2529be8f7a06750557759dbc0
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-08-25 16:43:58 +0530

    Update use-order.ts
    Signed-off-by: Krishna Baviskar <krishnabaviskar40@gmail.com>

--- src/hooks/use-order.ts
+++ src/hooks/use-order.ts
@@ -5,7 +5,7 @@ import { OrderContext, type OrderContextType } from "@/context/order-provider";
 
 export function useOrder(): OrderContextType {
   const context = useContext(OrderContext);
-  if (context === undefined) {
+   if (context === undefined) {
     throw new Error("useOrder must be used within a OrderProvider");
   }
   return context;
